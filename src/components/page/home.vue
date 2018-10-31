d5886240f8ce31049af1ad3ca517406ff5249214
wuzda <wuzd@mybilin.cn>
2018-10-31 19:29:36 +0800

    样式重置文件

--- src/components/page/home.vue
+++ src/components/page/home.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-      首页
-      <router-link to="/checkListPage">检查列表页</router-link>
-      <router-link to="/cloudListPage">云处方列表页</router-link>
-      <router-link to="/clinic">诊所页</router-link>
-      <router-link to="/home/personal">个人页</router-link>
+      <section class="home_top">
+        <div class="top_item">
+          <img src="../../assets/img/yf.png">
+          <span>云药房订单</span>
+        </div>
+        <div class="top_item">
+          <img src="../../assets/img/yf.png">
+          <span>云药房订单</span>
+        </div>
+        <div class="top_item">
+          <img src="../../assets/img/yf.png">
+          <span>云药房订单</span>
+        </div>
+      </section>
     </div>
 </template>
 
@@ -15,5 +24,26 @@
 </script>
 
 <style scoped>
-
+  .home_top{
+    display: flex;
+    justify-content: center;
+  }
+  .home_top .top_item{
+    flex: 1;
+    padding: 16px 0 18px 0;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+  }
+  .home_top .top_item span{
+    font-size: 14px;
+    color: #3F3F3F;
+    padding-top: 10px;
+  }
+  .home_top .top_item img{
+    width: 36px;
+    height: 36px;
+    margin: 0 auto;
+    display: inline-block;
+  }
 </style>
