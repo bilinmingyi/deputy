22ea8f40e60a2ea4d821eff9a95f8de13c1aca90
wuzda <wuzd@mybilin.cn>
2018-10-31 15:50:19 +0800

    样式重置文件

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,10 @@
 </script>
 
 <style>
-
-
+  @import "assets/css/reset.css";
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
   }
 </style>
