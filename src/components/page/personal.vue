363f193520ad167fbef6fba99cd6e375c096aab2
wuzda <wuzd@mybilin.cn>
2018-11-01 14:38:13 +0800

    修改

--- src/components/page/personal.vue
+++ src/components/page/personal.vue
@@ -1,15 +1,120 @@
 <template>
-    <div>
-      个人
-    </div>
+  <div>
+    <d-header :isCanBack="false">
+      个人中心
+    </d-header>
+    <section class="person_content">
+      <img class="person_portrait" src="../../assets/img/ndb.png">
+      <div class="person_infor">
+        <div>
+          <span class="person_name">李代表</span>
+          <span class="person_age">40岁</span>
+        </div>
+        <div class="person_id">帐号ID：12243</div>
+      </div>
+      <div>
+        <img class="person_change" src="../../assets/img/fanghui@2x.png"/>
+      </div>
+    </section>
+    <section class="person_list">
+      <ul>
+        <li>
+          <img src="../../assets/img/yj.png">
+          <span> 我的业绩</span>
+        </li>
+        <hr class="full-screen-hr">
+        <li>
+          <img src="../../assets/img/td.png">
+          我的团队
+        </li>
+      </ul>
+      <hr class="full-screen-hr">
+    </section>
+    <d-footer :activeItem="3"></d-footer>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "personal"
+  import dHeader from '@/components/common/dHeader.vue'
+  import dFooter from '@/components/common/dFooter.vue'
+
+  export default {
+    name: "personal",
+    components: {
+      'd-header': dHeader,
+      'd-footer': dFooter
     }
+  }
 </script>
 
 <style scoped>
+  .person_content {
+    margin-top: 44px;
+    background: #FFFFFF;
+    display: flex;
+    padding: 1.375rem 1.25rem;
+    align-items: center;
+    margin-bottom: 0.78rem;
+  }
 
+  .person_portrait {
+    height: 3.5rem;
+    width: 3.5rem;
+    margin-right: 0.625rem;
+  }
+
+  .person_infor {
+    flex: 1;
+    font-size: 1rem;
+    color: #7C7C7C;
+  }
+
+  .person_age {
+    background: #2CBBF4;
+    font-size: 0.75rem;
+    color: #FFFFFF;
+    padding: 0.03rem 0.315rem;
+    border-radius: 3.125rem;
+  }
+
+  .person_name {
+    color: #3F3F3F;
+    font-weight: bold;
+    margin-right: 0.75rem;
+  }
+
+  .person_id {
+    margin-top: 0.5rem;
+  }
+
+  .person_change {
+    height: 1.25rem;
+    -webkit-transform: rotate(180deg);
+    -moz-transform: rotate(180deg);
+    -ms-transform: rotate(180deg);
+    -o-transform: rotate(180deg);
+    transform: rotate(180deg);
+    padding-left: 0.5rem;
+  }
+
+  .person_list {
+    background: #FFFFFF;
+    font-size: 1rem;
+    color: #3F3F3F;
+  }
+
+  .person_list ul {
+    padding-left: 0.94rem;
+    padding-right: 0.94rem;
+  }
+
+  .person_list li {
+    display: flex;
+    padding: 0.815rem 0 0.78rem;
+  }
+
+  .person_list li img {
+    height: 1.25rem;
+    margin-right: 0.5rem;
+  }
 </style>
