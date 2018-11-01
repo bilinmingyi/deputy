38ce5e27665e7c6b11b9f97c6c390e344ed78e5d
wuzda <wuzd@mybilin.cn>
2018-11-01 10:07:13 +0800

    新增搜索组件

---
+++ src/components/common/dSearch.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="bg-ff">
+    <div class="search">
+      <div class="search_left">
+        <img src="../../assets/img/ss.png">
+        <input type="text" placeholder="诊所名称" v-model="searchName" @input="searchInput()"/>
+      </div>
+      <div>
+        <button class="search_btn">查询</button>
+      </div>
+    </div>
+    <hr class="full-screen-hr">
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "dSearch",
+    data:function () {
+      return{
+        searchName:"",
+        timer:""
+      }
+    },
+    methods:{
+      searchInput:function () {
+        var self=this;
+        clearTimeout(self.timer);
+        self.timer=setTimeout(function () {
+          self.$emit("on-search",self.searchName)
+        },300);
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  .search {
+    background: #ffffff;
+    display: flex;
+    padding: 0.5rem 0.94rem;
+  }
+
+  .search_left {
+    flex: 1;
+    background: #EDEDED;
+    border-radius: 0.25rem;
+    margin-right: 0.75rem;
+    padding: 0.375rem 0.75rem;
+    display: flex;
+  }
+
+  .search_left img {
+    height: 1.25rem;
+  }
+
+  .search_left input {
+    border: none;
+    background: transparent;
+    outline: none;
+    padding-left: 0.395rem;
+  }
+
+  .search_left input::-webkit-input-placeholder {
+    font-size: 0.875rem;
+    color: #8C8C8C;
+  }
+
+  .search_btn {
+    background: #08BAC6;
+    border-radius: 0.25rem;
+    font-size: 0.875rem;
+    color: #FFFFFF;
+    padding: 0.375rem 1rem;
+    border: none;
+  }
+</style>
--- src/components/page/clinic/clinicList.vue
+++ src/components/page/clinic/clinicList.vue
@@ -1,60 +1,75 @@
 <template>
-    <div>
-
-      <section class="clinic_content">
-        <ul>
-          <li>
-            <div class="order_infor">
-              <div class="order_infor_top">
-                <div>采购订单下单</div>
-                <div class="bg_EB">综合诊所</div>
-              </div>
-              <div class="order_infor_bottom">
-                深圳淳道门诊：这是消息这是消息这是消这是消息消息这是消息这是消息这是消这是消息消息
-              </div>
+  <div>
+    <d-search @on-search="getData"></d-search>
+    <section class="clinic_content">
+      <ul>
+        <li>
+          <div class="order_infor">
+            <div class="order_infor_top">
+              <div>采购订单下单</div>
+              <div class="bg_EB">综合诊所</div>
             </div>
-            <hr class="full-screen-hr">
-          </li>
-          <li>
-            <div class="order_infor">
-              <div class="order_infor_top">
-                <div>采购订单下单</div>
-                <div class="bg_ED">西医诊所</div>
-              </div>
-              <div class="order_infor_bottom">
-                深圳淳道门诊：这是消息这是消息这是消这是消息消息这是消息这是消息这是消这是消息消息
-              </div>
+            <div class="order_infor_bottom">
+              深圳淳道门诊：这是消息这是消息这是消这是消息消息这是消息这是消息这是消这是消息消息
             </div>
-            <hr class="full-screen-hr">
-          </li>
-          <li>
-            <div class="order_infor">
-              <div class="order_infor_top">
-                <div>采购订单下单</div>
-                <div class="bg_4D">西医诊所</div>
-              </div>
-              <div class="order_infor_bottom">
-                深圳淳道门诊：这是消息这是消息这是消这是消息消息这是消息这是消息这是消这是消息消息
-              </div>
+          </div>
+          <hr class="full-screen-hr">
+        </li>
+        <li>
+          <div class="order_infor">
+            <div class="order_infor_top">
+              <div>采购订单下单</div>
+              <div class="bg_ED">西医诊所</div>
             </div>
-            <hr class="full-screen-hr">
-          </li>
-        </ul>
-        <div class="add_more">查看更多...</div>
-      </section>
-    </div>
+            <div class="order_infor_bottom">
+              深圳淳道门诊：这是消息这是消息这是消这是消息消息这是消息这是消息这是消这是消息消息
+            </div>
+          </div>
+          <hr class="full-screen-hr">
+        </li>
+        <li>
+          <div class="order_infor">
+            <div class="order_infor_top">
+              <div>采购订单下单</div>
+              <div class="bg_4D">西医诊所</div>
+            </div>
+            <div class="order_infor_bottom">
+              深圳淳道门诊：这是消息这是消息这是消这是消息消息这是消息这是消息这是消这是消息消息
+            </div>
+          </div>
+          <hr class="full-screen-hr">
+        </li>
+      </ul>
+      <div class="add_more">查看更多...</div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "clinicList"
+  import dSearch from '@/components/common/dSearch.vue'
+  export default {
+    name: "clinicList",
+    data:function () {
+      return{
+
+      }
+    },
+    components:{
+      'd-search':dSearch
+    },
+    methods:{
+      getData:function (name) {
+        console.log(name)
+      }
     }
+  }
 </script>
 
 <style scoped>
-  .clinic_content{
+  .clinic_content {
     background: #ffffff;
   }
+
   .order_infor {
     padding: 0.625rem 1rem;
   }
@@ -63,13 +78,15 @@
     display: flex;
 
   }
-  .order_infor_top div:first-child{
+
+  .order_infor_top div:first-child {
 
     color: #3F3F3F;
     font-size: 1rem;
     font-weight: bolder;
   }
-  .order_infor_top div:last-child{
+
+  .order_infor_top div:last-child {
     font-size: 0.625rem;
     color: #FFFFFF;
     border-radius: 4px;
@@ -78,15 +95,19 @@
     padding: 0 0.25rem;
     margin-left: 0.5rem;
   }
-  .bg_EB{
+
+  .bg_EB {
     background: #EB6262;
   }
-  .bg_ED{
+
+  .bg_ED {
     background: #EDAB15;
   }
-  .bg_4D{
+
+  .bg_4D {
     background: #4DBC89;
   }
+
   .order_infor_bottom {
     margin-top: 0.5rem;
     color: #7C7C7C;
@@ -95,7 +116,8 @@
     overflow: hidden;
     text-overflow: ellipsis;
   }
-  .add_more{
+
+  .add_more {
     color: #7C7C7C;
     font-size: 0.875rem;
     background: #FAFAFA;
