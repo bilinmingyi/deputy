1bd79604087a70aa0e6b32d854004cfc9573ec21
Bilin008 <liezh@mybilin.com>
2018-11-01 11:25:40 +0800

    infoBar

---
+++ src/components/common/infoBar.vue
@@ -0,0 +1,104 @@
+<template>
+  <section>
+    <div class="info-header">
+      <span class="info-header-text">{{infoTitle}}</span>
+      <slot name='btn' class="info-header-btn"></slot>
+    </div>
+    <div class="info-content">
+      <slot>
+        <!-- <div>
+          <span>就诊时间：</span>
+          <span>2018-12-23 22:26</span>
+        </div>
+        <div>
+          <span>详细地址：</span>
+          <span>王广州市天河区珠江新城花城汇XXXxxxx街道</span>
+        </div> -->
+      </slot>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      infoTitle: this.title
+    };
+  }
+};
+</script>
+
+<style scoped>
+.info-header {
+  background: #fff;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+}
+.info-header::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-bottom: 1px solid #d9d9d9;
+  transform: scaleY(.5);
+}
+.info-header-text {
+  font-family: PingFangSC-Medium;
+  font-size: 1rem;
+  color: #3f3f3f;
+  letter-spacing: 0;
+  font-weight: 600;
+  line-height: 1.25rem;
+  margin-left: 0.75rem;
+  border-left: 0.25rem solid #08bac6;
+  padding-left: 0.5rem; 
+}
+.info-header button {
+  position: absolute;
+  right: 0.94rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #ebf8f9;
+  border: 1px solid #08bac6;
+  border-radius: 0.25rem;
+  padding: 0 0.5rem;
+  min-height: 2rem;
+  font-family: PingFangSC-Regular;
+  font-size: 0.75rem;
+  color: #3f3f3f;
+  letter-spacing: 0;
+  text-align: left;
+}
+.info-content {
+  padding: 0.5rem 0.9375rem;
+  margin-bottom: 0.75rem;
+  background: #fff;
+}
+.info-content > * {
+  display: flex;
+}
+.info-content > * > *:nth-child(1) {
+  line-height: 1.5rem;
+  min-width: 20vw;
+  font-family: PingFangSC-Regular;
+  font-size: 0.9375rem;
+  color: #7C7C7C;
+  letter-spacing: 0;
+}
+.info-content > * > *:nth-child(2) {
+  flex: 1;
+  font-family: PingFangSC-Regular;
+  line-height: 1.5rem;
+  font-size: 0.9375rem;
+  color: #000;
+}
+</style>
--- src/components/page/cloudorder/cloudlist.vue
+++ src/components/page/cloudorder/cloudlist.vue
@@ -1,22 +1,60 @@
 <template>
     <div>
       <d-header>云处方订单</d-header>
-      <span>这是云处方列表</span>
-      <d-footer :activeItem="2"></d-footer>
+      <section class="content">
+        <info-bar :title="'患者详情'">
+          <div>
+            <span>就诊时间：</span>
+            <span>{{order.time}}</span>
+          </div>
+          <div>
+            <span>患者姓名：</span>
+            <span>{{order.patientName}}</span>
+          </div>
+          <div>
+            <span>联系电话：</span>
+            <span>{{order.mobile}}</span>
+          </div>
+          <div>
+            <span>就诊医生：</span>
+            <span>{{order.doctor}}</span>
+          </div>
+        </info-bar>
+        <info-bar :title="'配送地址'">
+          <button slot="btn">选择地址</button>
+        </info-bar>
+      </section>
+      <!-- <d-footer :activeItem="2"></d-footer> -->
     </div>
 </template>
 
 <script>
 import dHeader from "@/components/common/dHeader";
 import dFooter from "@/components/common/dFooter";
+import infoBar from "@/components/common/infoBar";
 export default {
-  name: "cloudlist",
   components: {
     dHeader,
-    dFooter
+    dFooter,
+    infoBar
+  },
+  data() {
+    return {
+      order: {
+        time: '2018-12-23 22:26',
+        patientName: '王尼玛',
+        mobile: '13245678901',
+        doctor: '李教授'
+      }
+    }
   }
 };
 </script>
 
 <style scoped>
+.content {
+  height: calc(100vh - 2.75rem);
+  width: 100vw;
+  margin: 2.75rem 0 0 0;
+}
 </style>
