6fdd84db87f672989fa086b173e3bf90d5580f6d
wuzda <wuzd@mybilin.cn>
2018-11-02 15:01:56 +0800

    新增

--- src/components/page/checkorder/clinicCheckOrderPage.vue
+++ src/components/page/checkorder/clinicCheckOrderPage.vue
@@ -10,7 +10,25 @@
       <hr class="full-screen-hr">
     </div>
     <section class="mt92 mb50">
-      <div class="order_item">
+
+      <router-link to="/checkListPage/clinicCheckOrderPage/confirmedOrderDetail/1212">
+        <div class="order_item">
+          <div class="order_item_top">
+            <span>李教授</span>
+            <span>18/10/01 14:12</span>
+          </div>
+          <div class="order_item_middle">
+            <span>王尼玛</span>
+            <span>待确认</span>
+          </div>
+          <div class="order_item_bottom">
+            <span>付款金额：323</span>
+          </div>
+          <hr class="full-screen-hr">
+        </div>
+      </router-link>
+      <router-link to="/checkListPage/clinicCheckOrderPage/confirmedOrderDetail/1212">
+        <div class="order_item">
         <div class="order_item_top">
           <span>李教授</span>
           <span>18/10/01 14:12</span>
@@ -24,20 +42,7 @@
         </div>
         <hr class="full-screen-hr">
       </div>
-      <div class="order_item">
-        <div class="order_item_top">
-          <span>李教授</span>
-          <span>18/10/01 14:12</span>
-        </div>
-        <div class="order_item_middle">
-          <span>王尼玛</span>
-          <span>待确认</span>
-        </div>
-        <div class="order_item_bottom">
-          <span>付款金额：323</span>
-        </div>
-        <hr class="full-screen-hr">
-      </div>
+      </router-link>
       <div class="add_more">查看更多...</div>
     </section>
     <div class="record_sheet">
--- src/components/page/cloudorder/waitPayPage.vue
+++ src/components/page/cloudorder/waitPayPage.vue
@@ -1,19 +1,131 @@
 <template>
-    <div>
-      <d-header>订单详情</d-header>
-    </div>
+  <div>
+    <d-header>订单详情</d-header>
+    <section class="content">
+      <info-bar :title="'患者详情'">
+        <div>
+          <span>就诊时间：</span>
+          <span>{{order.time}}</span>
+        </div>
+        <div>
+          <span>患者姓名：</span>
+          <span>{{order.patientName}}</span>
+        </div>
+        <div>
+          <span>联系电话：</span>
+          <span>{{order.mobile}}</span>
+        </div>
+        <div>
+          <span>就诊医生：</span>
+          <span>{{order.doctor}}</span>
+        </div>
+      </info-bar>
+      <info-bar :title="'配送地址'">
+        <button slot="btn">选择地址</button>
+        <div>
+          <span>联系人名：</span>
+          <span>{{order.time}}</span>
+        </div>
+        <div>
+          <span>联系电话：</span>
+          <span>{{order.patientName}}</span>
+        </div>
+        <div>
+          <span>详细地址：</span>
+          <span>{{order.mobile}}</span>
+        </div>
+        <div>
+          <span>配送金额：</span>
+          <span style="color: red">￥12</span>
+        </div>
+      </info-bar>
+      <info-header>处方信息</info-header>
+      <div class="bg-fff p16 mb12">
+        <table class="d-table" data-type="1">
+          <tbody>
+          <tr>
+            <th>处方类型</th>
+            <th>发药类型</th>
+            <th>金额</th>
+          </tr>
+          <tr>
+            <td>中药饮片</td>
+            <td>云药房</td>
+            <td>123</td>
+          </tr>
+          <tr>
+            <td>颗粒饮片</td>
+            <td>本机构</td>
+            <td>456</td>
+          </tr>
+          <tr>
+            <td>附加服务</td>
+            <td>云药房</td>
+            <td>789</td>
+          </tr>
+          <tr>
+            <td>中药饮片</td>
+            <td>本机构</td>
+            <td>012</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="payment">
+        <span>付款金额</span>
+        <span>￥6666</span>
+      </div>
+      <div class="pay_operation">
+        <button>关闭</button>
+        <button>取消订单</button>
+        <button>去支付</button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
   import dHeader from "@/components/common/dHeader.vue"
-    export default {
-        name: "waitPayPage",
-      components:{
-          dHeader
+  import infoBar from "@/components/common/infoBar";
+  import infoHeader from "@/components/common/infoHeader";
+  export default {
+    name: "waitPayPage",
+    data(){
+      return{
+        order: {
+          time: "2018-12-23 22:26",
+          patientName: "王尼玛",
+          mobile: "13245678901",
+          doctor: "李教授"
+        },
       }
+    },
+    components: {
+      dHeader,
+      infoHeader,
+      infoBar
     }
+  }
 </script>
 
 <style scoped>
+  .content {
+    min-height: calc(100vh - 2.75rem);
+    width: 100vw;
+    margin: 2.75rem 0 0 0;
+  }
+  .payment{
+    background: #FFFFFF;
+    color: #7C7C7C;
+    font-size: 1rem;
+    padding: 0.78rem 0.94rem;
+    margin-bottom: 4.25rem;
+  }
+  .payment span:last-child{
+    margin-left: 1rem;
+    color: #EB6262;
+    font-weight: bold;
+  }
+
 
 </style>
