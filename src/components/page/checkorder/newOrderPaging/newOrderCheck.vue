b2d8a2fa7da1c9559fa5ac2b2fa1fc0ead86c580
wuzda <wuzd@mybilin.cn>
2018-11-07 15:02:39 +0800

    检验录单页面组件分块

---
+++ src/components/page/checkorder/newOrderPaging/newOrderCheck.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <info-header>
+      检验项目
+      <button slot="btn">添加项目</button>
+    </info-header>
+    <div class="bg-fff p15 mb12">
+      <touch-list :data="touchListData">
+        <div slot-scope="{prop}">
+          <span>{{prop.name}}</span>
+        </div>
+      </touch-list>
+    </div>
+  </div>
+</template>
+
+<script>
+  import infoHeader from "@/components/common/infoHeader";
+  import touchList from "@/components/common/touchList";
+
+  export default {
+    name: "orderCheck",
+    components: {
+      infoHeader,
+      touchList
+    },
+    data(){
+      return {
+        touchListData: [
+          {
+            name: "甲功三项",
+            age: 12
+          },
+          {
+            name: "血常规",
+            age: 34
+          },
+          {
+            name: "肾功能常规",
+            age: 56
+          }
+        ],
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/page/checkorder/newOrderPaging/newOrderPatient.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <info-header>患者信息</info-header>
+    <div class="bg-fff pl15 pr15 mb12">
+      <div class="pay_line_two">
+        <span class="pr16">所属医生</span>
+        <select class="doctor_select">
+          <option value="">请选择所属医生</option>
+          <option>刁医生</option>
+        </select>
+      </div>
+      <hr class="full-screen-hr">
+      <div class="pay_line_two">
+        <span class="pr16">患者姓名</span>
+        <span>王尼玛</span>
+      </div>
+      <hr class="full-screen-hr">
+      <div class="pay_line_two">
+        <span class="pr16">患者电话</span>
+        <span>15876789544</span>
+      </div>
+      <hr class="full-screen-hr">
+      <div class="pay_line_two">
+        <span class="pr16">患者性别</span>
+        <span>
+            <d-radio :name="'sex'" :id="'sex-man'" :checkedVal=1 :curVal="1">男</d-radio>
+            <d-radio :name="'sex'" :id="'sex-woman'" :checkedVal=2>女</d-radio>
+            <d-radio :name="'sex'" :id="'sex-no'" :checkedVal=0>保密</d-radio>
+          </span>
+      </div>
+      <hr class="full-screen-hr">
+      <div class="pay_line_two">
+        <span class="pr16">患者年龄</span>
+        <div>
+          <input type="number" class="patient_age"/>岁
+          <input type="number" class="patient_age"/>月
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import infoHeader from "@/components/common/infoHeader";
+  import dRadio from "@/components/common/dRadio";
+
+  export default {
+    name: "newOrderPatient",
+    components:{
+      infoHeader,
+      dRadio
+    }
+  }
+</script>
+
+<style scoped>
+  .pay_line_two {
+    font-size: 1rem;
+    color: #3F3F3F;
+    padding: 0.78rem 0;
+    display: flex;
+  }
+
+  .doctor_select {
+    border: none;
+    outline: none;
+    font-size: 1rem;
+    color: #7C7C7C;
+    flex: 1;
+  }
+
+  .pay_line_two >>> .d-radio {
+    margin-right: 1.5rem;
+  }
+
+  .patient_age {
+    border: none;
+    width: 2rem;
+    outline: none;
+    text-align: center;
+    font-size: 1rem;
+    color: #3C3C3C;
+  }
+</style>
